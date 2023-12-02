812e1593310a65a96337bdaa125c39280ea9156c
Andreea Urian <andreea.urian@icloud.com>
2023-12-03 01:57:15 +0200

    AddProductWorkflow done

--- ShoppingApp.Domain/Workflows/AddProductsWorkflow.cs
+++ ShoppingApp.Domain/Workflows/AddProductsWorkflow.cs
@@ -1,11 +1,61 @@
 ﻿using System;
+using Data;
+using ShoppingApp.Domain.Services;
+using static ShoppingApp.Domain.Models.Cart;
+
 namespace ShoppingApp.Domain.Workflows
 {
-	public class AddProductsWorkflow
-	{
-		public AddProductsWorkflow()
-		{
-		}
-	}
-}
+    public class AddProductsWorkflow
+    {
+        private readonly ShoppingAppDbContext _dbContext;
 
+        public AddProductsWorkflow(ShoppingAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> Execute(string accountID, string product)
+        {
+            ICart searchedCart = await CartsRepository.GetCart(accountID);
+
+            bool succeded = false;
+            searchedCart.Match(
+                whenEmptyCart: @event =>
+                {
+                    Task.Run(async () =>
+                    {
+                        await CartsRepository.ChangeCartState(accountID, new PendingCart());
+                        succeded = await CartsRepository.AddNewProduct(accountID, product);
+                    });
+
+                    return @event;
+                },
+                whenPendingCart: @event =>
+                {
+                    Task.Run(async () =>
+                    {
+                        succeded = await CartsRepository.AddNewProduct(accountID, product);
+                    });
+
+                    return @event;
+                },
+                whenValidatedCart: @event =>
+                {
+                    succeded = false;
+                    return @event;
+                },
+                whenCalculatedCart: @event =>
+                {
+                    succeded = false;
+                    return @event;
+                },
+                whenPaidCart: @event =>
+                {
+                    succeded = false;
+                    return @event;
+                }
+            );
+
+            return succeded;
+        }
+    }
+}
