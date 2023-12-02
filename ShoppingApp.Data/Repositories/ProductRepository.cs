e59a8c14e546a3381816e021a9254b0ebb338ff1
cataususan <cataususan@gmail.com>
2023-12-03 00:30:46 +0200

    Making cart logic and product validation logic

---
+++ ShoppingApp.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ShoppingAppDbContext _dbContext;
+
+        public ProductRepository(ShoppingAppDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<ProductDTO> SearchProduct(string? productCode)
+        {
+            ProductDTO productDTO = await _dbContext.Products.FirstOrDefaultAsync(product => product.Uid == productCode);
+            return productDTO;
+        }
+    }
+}
