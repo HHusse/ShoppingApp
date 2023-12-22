126bdf578c5d564a57ed0b62a83e454556641f72
Hossein Vahid Azari <VahidAzariHossein@gmail.com>
2023-12-22 15:01:28 +0200

    Create an account utiles for testing

---
+++ ShoppingApp.Test/Utils/AccountTestUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ShoppingApp.Data.Models;
+
+namespace ShoppingApp.Test.Utils
+{
+    public static class AccountTestUtils
+    {
+        public static void CreateFakeAccounts(ref List<AccountDTO> accounts)
+        {
+            accounts.Add(new AccountDTO
+            {
+                Uid = Guid.NewGuid().ToString(),
+                LastName = "Vahid",
+                FirstName = "Hossein",
+                Email = "husse@gmail.com",
+                PhoneNumber = "+40712345678",
+                Address = "STR. Test",
+                Password = BCrypt.Net.BCrypt.HashPassword("1234")
+            });
+
+            accounts.Add(new AccountDTO
+            {
+                Uid = Guid.NewGuid().ToString(),
+                LastName = "user2",
+                FirstName = "user2",
+                Email = "user2@gmail.com",
+                PhoneNumber = "+40712345678",
+                Address = "STR. Test2",
+                Password = BCrypt.Net.BCrypt.HashPassword("1234")
+            });
+
+            accounts.Add(new AccountDTO
+            {
+                Uid = Guid.NewGuid().ToString(),
+                LastName = "user3",
+                FirstName = "user3",
+                Email = "user3@gmail.com",
+                PhoneNumber = "+40712345678",
+                Address = "STR. Test3",
+                Password = BCrypt.Net.BCrypt.HashPassword("1234")
+            });
+        }
+    }
+}
+
